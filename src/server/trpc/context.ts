fc3da106dc389611f9c199d0fefcd7d731b5a032
Korneliusz Łapacz <lapacz.kornel@gmail.com>
2022-11-24 20:19:49 +0100

    refactor

--- src/server/trpc/context.ts
+++ src/server/trpc/context.ts
@@ -3,7 +3,7 @@ import { type CreateNextContextOptions } from "@trpc/server/adapters/next";
 import { type Session } from "next-auth";
 
 import { getServerAuthSession } from "../common/get-server-auth-session";
-import { prisma } from "../db/client";
+import { prisma } from "../db";
 
 type CreateContextOptions = {
   session: Session | null;
